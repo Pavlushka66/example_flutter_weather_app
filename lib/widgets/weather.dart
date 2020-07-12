b547d9cdaccbe71a3bbe1142d4c44f3e23c8abff
Pavlushka66 <pavlushka66@gmail.com>
2020-07-12 14:15:53 -0500

    Trying to separate the logic from the widget in the Weather widget.

--- lib/widgets/weather.dart
+++ lib/widgets/weather.dart
@@ -6,78 +6,110 @@ import 'package:flutter_weather/blocs/blocs.dart';
 
 class Weather extends StatefulWidget {
   @override
-  State<Weather> createState() => _WeatherState();
+  State<Weather> createState() => _WeatherController();
 }
 
-class _WeatherState extends State<Weather> {
+class _WeatherController extends State<Weather> {
+  WeatherBloc weatherBloc;
+  ThemeBloc themeBloc;
+
   @override
   Widget build(BuildContext context) {
-    final weatherBloc = BlocProvider.of<WeatherBloc>(context);
-    final themeBloc = BlocProvider.of<ThemeBloc>(context);
+    weatherBloc = BlocProvider.of<WeatherBloc>(context);
+    themeBloc = BlocProvider.of<ThemeBloc>(context);
+    return _WeatherView(this);
+  }
+
+  @override
+  void dispose() {
+    weatherBloc.close();
+    themeBloc.close();
+    weatherBloc = null;
+    themeBloc = null;
+
+    super.dispose();
+  }
+
+  void handleCitySelection() async {
+    final city = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CitySelection(),
+      ),
+    );
+    if (city != null) {
+      weatherBloc.add(WeatherRequested(city: city));
+    }
+  }
+
+  void handleOpenSettings() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Settings(),
+      ),
+    );
+  }
+
+  Widget buildWeatherBloc() {
+    return BlocConsumer<WeatherBloc, WeatherState>(
+      listener: (context, state) {
+        if (state is WeatherLoadSuccess) {
+          themeBloc.add(
+            WeatherChanged(condition: state.weather.condition),
+          );
+        }
+      },
+      builder: (context, state) {
+        if (state is WeatherInitial) {
+          return Center(
+              child:
+                  Text('Please Select a Location')); // TODO: Move to resources
+        }
+        if (state is WeatherLoadInProgress) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (state is WeatherLoadSuccess) {
+          final weather = state.weather;
+
+          return BlocBuilder<ThemeBloc, ThemeState>(
+            builder: (context, themeState) {
+              return WeatherInfo(weather: weather, color: themeState.color);
+            },
+          );
+        }
+        return Text(
+          'Something went wrong!', // TODO: Move to resources
+          style: TextStyle(color: Colors.red),
+        );
+      },
+    );
+  }
+}
+
+class _WeatherView extends StatelessWidget {
+  final _WeatherController state;
+
+  const _WeatherView(this.state, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Weather'), // TODO: Move to resources
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.settings),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Settings(),
-                ),
-              );
-            },
+            onPressed: state.handleOpenSettings,
           ),
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () async {
-              final city = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CitySelection(),
-                ),
-              );
-              if (city != null) {
-                weatherBloc.add(WeatherRequested(city: city));
-              }
-            },
+            onPressed: state.handleCitySelection,
           )
         ],
       ),
       body: Center(
-        child: BlocConsumer<WeatherBloc, WeatherState>(
-          listener: (context, state) {
-            if (state is WeatherLoadSuccess) {
-              themeBloc.add(
-                WeatherChanged(condition: state.weather.condition),
-              );
-            }
-          },
-          builder: (context, state) {
-            if (state is WeatherInitial) {
-              return Center(
-                  child: Text(
-                      'Please Select a Location')); // TODO: Move to resources
-            }
-            if (state is WeatherLoadInProgress) {
-              return Center(child: CircularProgressIndicator());
-            }
-            if (state is WeatherLoadSuccess) {
-              final weather = state.weather;
-
-              return BlocBuilder<ThemeBloc, ThemeState>(
-                builder: (context, themeState) {
-                  return WeatherInfo(weather: weather, color: themeState.color);
-                },
-              );
-            }
-            return Text(
-              'Something went wrong!', // TODO: Move to resources
-              style: TextStyle(color: Colors.red),
-            );
-          },
-        ),
+        child: state.buildWeatherBloc(),
       ),
     );
   }
